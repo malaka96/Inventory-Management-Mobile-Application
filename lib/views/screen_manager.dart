b5719759c741c8b90a8ac1dbdf21c1b1b08ad4e1
malaka96 <madhubhashana655@gmail.com>
2026-04-02 11:54:47 +0530

    setup bottom navigation bar

---
+++ lib/views/screen_manager.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:inventory_management_mobile_app/views/main_screens/history_screen.dart';
+import 'package:inventory_management_mobile_app/views/main_screens/home_screen.dart';
+import 'package:inventory_management_mobile_app/views/main_screens/products_screen.dart';
+import 'package:inventory_management_mobile_app/views/main_screens/setting_screen.dart';
+
+class ScreenManager extends StatefulWidget {
+  const ScreenManager({super.key});
+
+  @override
+  State<ScreenManager> createState() => _ScreenManagerState();
+}
+
+class _ScreenManagerState extends State<ScreenManager> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    ProductsScreen(),
+    HistoryScreen(),
+    SettingScreen(),
+  ];
+
+  void changeTab(int index) {
+    if (index < 0 || index >= _screens.length) return;
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: changeTab,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.normal,
+        ),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.storefront_rounded),
+            label: "Products",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: "History",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_rounded),
+            label: "Settings",
+          ),
+        ],
+      ),
+    );
+  }
+}
