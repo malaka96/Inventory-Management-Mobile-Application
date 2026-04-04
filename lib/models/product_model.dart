4bc6b7c1ffffee07672dd64dc84341a67afcf110
malaka96 <madhubhashana655@gmail.com>
2026-04-04 21:35:28 +0530

    create product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,14 @@
+class ProductModel {
+  int id = 0;
+  String name;
+  int quatity;
+  String category;
+  int minStock;
+
+  ProductModel({
+    required this.name,
+    required this.quatity,
+    required this.category,
+    required this.minStock,
+  });
+}
