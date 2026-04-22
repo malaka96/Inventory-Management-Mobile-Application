b54aa363f6ecf3e1ebe5e266a317b4b109699822
malaka96 <madhubhashana655@gmail.com>
2026-04-22 20:32:09 +0530

    setup hive service for product status model

--- lib/core/constants/hive_boxes.dart
+++ lib/core/constants/hive_boxes.dart
@@ -1,3 +1,4 @@
 class HiveBoxes {
   static const String product = 'products_box';
+  static const String productStatus = 'product_status_box';
 }
--- lib/core/services/hive_service.dart
+++ lib/core/services/hive_service.dart
@@ -1,6 +1,7 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:inventory_management_mobile_app/core/constants/hive_boxes.dart';
 import 'package:inventory_management_mobile_app/data/models/product_model.dart';
+import 'package:inventory_management_mobile_app/data/models/product_status_model.dart';
 
 class HiveService {
   static Future<void> init() async {
@@ -9,6 +10,10 @@ class HiveService {
     if (!Hive.isAdapterRegistered(1)) {
       Hive.registerAdapter(ProductModelAdapter());
     }
+    if (!Hive.isAdapterRegistered(2)) {
+      Hive.registerAdapter(ProductStatusModelAdapter());
+    }
     await Hive.openBox<ProductModel>(HiveBoxes.product);
+    await Hive.openBox<ProductStatusModel>(HiveBoxes.productStatus);
   }
 }
